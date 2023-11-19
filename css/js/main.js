9ede98cc5e399b0528d4a9c360949f9038207b1a
Joy Ame <104561447+JoyAme@users.noreply.github.com>
2023-11-19 05:50:08 +0100

    update

---
+++ css/js/main.js
@@ -0,0 +1,14 @@
+
+const menu=document.getElementById('menu-bar')
+
+const Click=()=>{
+
+}
+
+
+function Click(){
+    const Bar=document.querySelector('.barz');
+    Bar.classList.toggle('active')
+}
+
+
