731f81736ba1fa90143b332291a1a9dd2c1900fd
Joy Ame <104561447+JoyAme@users.noreply.github.com>
2023-12-04 21:25:33 +0100

    update

--- profilepage/profile.js
+++ profilepage/profile.js
@@ -1,20 +1,20 @@
 let menu=document.getElementById('menu-bar');
 let closee=document.getElementById('close-bar');
 let leftBar=document.querySelector('.left-bar')
+let log=document.querySelector('.log')
 
 function OpenMenu(){
    console.log('great')
    leftBar.style.display='block';
-    closee.style.display='block';
-    menu.style.display='none';
+    // log.classList.toggle=('active')
+  
 }
 
 
 function CloseMenu(){
    
     leftBar.style.display='none'
-    menu.style.display='block'
-    closee.style.display='none'
+    log.style.display='block'
 }
 
 
